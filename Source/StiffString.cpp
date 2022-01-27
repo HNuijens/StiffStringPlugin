18bf2a03dec2bb98cbfdfb6bc24e89e66f0ab807
Helmer Nuijens <helmernuijens@gmail.com>
2022-01-27 10:44:43 +0100

    init commit

---
+++ Source/StiffString.cpp
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    StiffString.cpp
+    Created: 27 Jan 2022 10:24:42am
+    Author:  helme
+
+  ==============================================================================
+*/
+
+#include "StiffString.h"
+
+
+StiffString::StiffString()
+{
+
+}
+
+StiffString::~StiffString()
+{
+
+}
---
+++ Source/StiffString.h
@@ -0,0 +1,25 @@
+/*
+  ==============================================================================
+
+    StiffString.h
+    Created: 27 Jan 2022 10:24:42am
+    Author:  helme
+
+  ==============================================================================
+*/
+
+#pragma once
+
+class StiffString
+{
+
+public:
+    StiffString();      // Constructor
+    ~StiffString();     // Destructor
+
+    
+
+
+private:
+
+};
