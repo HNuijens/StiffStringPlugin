9507bc53c587be6c9c0dd78677df6b814a05f732
HNuijens <helmernuijens@gmail.com>
2022-03-03 16:08:27 +0100

    added bow cpp and header file

---
+++ Source/Bow.h
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    Bow.h
+    Created: 3 Mar 2022 3:57:03pm
+    Author:  Helmer Nuijens
+
+  ==============================================================================
+*/
+#include <JuceHeader.h>
+
+class Bow
+{
+
+public:
+    Bow();      // Constructor
+    ~Bow();     // Destructor
+    
+    void setBowParams();
+    void computeExcitation();
+
+
+private:
+
+};
+#pragma once
+
+
